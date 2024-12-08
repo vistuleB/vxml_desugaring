99c57aadd3647bdc970b34c91272e4af1aa775c8
MrChaker <mr.chakerw@gmail.com>
2024-12-08 17:26:58 +0100

    Update build for use with solid

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ gleam build
 gleam run -m gleescript
 
 rm -r ../../Devland-agancy/little-bo-peep-leptos/parser
-mv ./lbp_desugaring ../../Devland-agancy/little-bo-peep-leptos/parser
+rm -r ../../MrChaker/little-bo-peep-solid/parser_script
+
+cp ./lbp_desugaring ../../Devland-agancy/little-bo-peep-leptos/parser
+mv ./lbp_desugaring ../../MrChaker/little-bo-peep-solid/parser_script
