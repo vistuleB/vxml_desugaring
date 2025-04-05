b0073e5570a2c14dff92deb8e293e4c596ea58ba
JOHN STEINBERGER <jpsteinb@gmail.com>
2025-04-05 13:43:57 +0200

    remove build.sh and spotlight executable

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-gleam build
-gleam run -m gleescript
-
-rm -r ../../Devland-agancy/little-bo-peep-leptos/parser
-rm -r ../../MrChaker/little-bo-peep-solid/parser_script
-
-cp ./lbp_desugaring ../../Devland-agancy/little-bo-peep-leptos/parser
-mv ./lbp_desugaring ../../MrChaker/little-bo-peep-solid/parser_script
